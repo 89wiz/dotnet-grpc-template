9556563b91202cc7dd16b30cd671c897cb330f6b
89wiz <lgs_scout@hotmail.com>
2024-05-26 20:43:53 -0300

    minor clean-up

--- gRPCTemplate.gRPC/Services/AuthService.cs
+++ gRPCTemplate.gRPC/Services/AuthService.cs
@@ -1,30 +1,13 @@
 ﻿using Grpc.Core;
-using gRPCTemplate.Application.User.Login;
 using gRPCTemplate.gRPC.Mapping;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 
 namespace gRPCTemplate.gRPC.Services;
 
-//[AllowAnonymous]
 public class AuthService(ISender sender) : Auth.AuthBase
 {
     private readonly ISender sender = sender;
-
-    [AllowAnonymous]
-    public override Task<EchoResponse> Echo(EchoRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new EchoResponse { Message = request.Message });
-    }
-
-    public override async Task AsyncEcho(IAsyncStreamReader<EchoRequest> requestStream, IServerStreamWriter<EchoResponse> responseStream, ServerCallContext context)
-    {
-        while (await requestStream.MoveNext())
-        {
-            await responseStream.WriteAsync(new EchoResponse { Message = requestStream.Current.Message });
-        }
-        return;
-    }
 
     [AllowAnonymous]
     public override async Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
